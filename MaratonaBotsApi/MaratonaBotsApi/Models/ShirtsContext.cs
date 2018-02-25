71d1948b921fd919ed83f06a8c257cfefbf11ef5
Jaqueline Lemes Botaro <jlemesbotaro@gmail.com>
2018-02-24 21:41:42 -0300

    Adicionando controlador, modelo e contexto para Shirts

---
+++ MaratonaBotsApi/MaratonaBotsApi/Models/ShirtsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MaratonaBotsApi.Models
+{
+    public class ShirtsContext : DbContext
+    {
+        public ShirtsContext(DbContextOptions<ShirtsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Shirt> Shirts { get; set; }
+    }
+}
