6c49cc0cc7389c31441051369c9f5342e605caa8
Jaqueline Lemes Botaro <jlemesbotaro@gmail.com>
2018-03-11 21:45:31 -0300

    Atualizando migração inicial e ativando update

--- ManicureEmCasaApi/ManicureEmCasaApi/Migrations/Configuration.cs
+++ ManicureEmCasaApi/ManicureEmCasaApi/Migrations/Configuration.cs
@@ -6,19 +6,24 @@ namespace ManicureEmCasaApi.Migrations
     using System.Linq;
     using ManicureEmCasaApi.Models;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<ManicureEmCasaApi.Models.AppointmentContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ManicureEmCasaApi.Models.ManicureEmCasaContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(ManicureEmCasaApi.Models.AppointmentContext context)
+        protected override void Seed(ManicureEmCasaApi.Models.ManicureEmCasaContext context)
         {
             context.Appointments.AddOrUpdate(x => x.Id,
-            new Appointment() { Id = 1, Name = "Jane Chang", Phone = "19 9999-9999", Email = "janechang@example.com", DayTime = new DateTime(2018, 3, 15, 9, 50, 0), PaymentType = PaymentTypes.Money, Service = Services.Manicure, Place = "Rua 1, 125, Jd. do Sol, Campinas - SP" },
-            new Appointment() { Id = 1, Name = "Jane Chang", Phone = "19 9999-9999", Email = "janechang@example.com", DayTime = new DateTime(2018, 4, 30, 10, 50, 0), PaymentType = PaymentTypes.DebitCard, Service = Services.Pedicure, Place = "Rua 1, 125, Jd. do Sol, Campinas - SP" },
-            new Appointment() { Id = 1, Name = "Jane Chang", Phone = "19 9999-9999", Email = "janechang@example.com", DayTime = new DateTime(2018, 3, 20, 13, 50, 0), PaymentType = PaymentTypes.Money, Service = Services.Completo, Place = "Rua 1, 125, Jd. do Sol, Campinas - SP" }
+            new Appointment() { Id = 1, Name = "Jane Chang", Phone = "19 9999-9999", Email = "janechang@example.com", DayTime = new DateTime(2018, 3, 15, 9, 50, 0), PaymentType = PaymentTypes.Money, Service = ServiceTypes.Manicure, Place = "Rua 1, 125, Jd. do Sol, Campinas - SP" },
+            new Appointment() { Id = 2, Name = "Jane Chang", Phone = "19 9999-9999", Email = "janechang@example.com", DayTime = new DateTime(2018, 4, 30, 10, 50, 0), PaymentType = PaymentTypes.DebitCard, Service = ServiceTypes.Pedicure, Place = "Rua 1, 125, Jd. do Sol, Campinas - SP" },
+            new Appointment() { Id = 3, Name = "Jane Chang", Phone = "19 9999-9999", Email = "janechang@example.com", DayTime = new DateTime(2018, 3, 20, 13, 50, 0), PaymentType = PaymentTypes.Money, Service = ServiceTypes.Completo, Place = "Rua 1, 125, Jd. do Sol, Campinas - SP" }
+        );
+            context.MyServices.AddOrUpdate(x => x.Id,
+            new Service() { Id = 1, Name = "Manicure", Price = ServicePrices.Manicure },
+            new Service() { Id = 2, Name = "Pedicure", Price = ServicePrices.Pedicure },
+            new Service() { Id = 3, Name = "Completo - Manicure e Pedicure", Price = ServicePrices.Completo }
         );
         }
     }
